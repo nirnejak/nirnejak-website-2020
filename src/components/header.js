933fc22c19e973fcc50baea5980e63b092f341e6
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2020-01-20 00:06:34 +0530

    Project Setup

--- src/components/header.js
+++ src/components/header.js
@@ -3,31 +3,12 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+  <header>
+    <nav>
+      <div className="container">
+        <h2>{siteTitle}</h2>
+      </div>
+    </nav>
   </header>
 )
 
