379c7d7259955ea152d874d58452b76927e044ea
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2020-02-19 02:32:07 +0530

    Missing Icon Added

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ function footer() {
     <div className="has-text-centered" style={{ margin: "200px 10px" }}>
       <div className="is-size-3 has-text-weight-bold has-text-black">Follow Me on</div>
 
-      <div style={{ margin: "30 0px" }}>
+      <div style={{ marginBottom: 10, marginTop: 10 }}>
         <svg width="64" height="5" viewBox="0 0 64 5" fill="none" xmlns="http://www.w3.org/2000/svg">
           <rect width="64" height="5" fill="#027BFF" />
         </svg>
