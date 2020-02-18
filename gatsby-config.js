9b2dd5d4be3ba4cf715b1589c40b97c2d8417fe5
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2020-02-18 23:40:30 +0530

    Logo Added

--- gatsby-config.js
+++ gatsby-config.js
@@ -80,7 +80,7 @@ module.exports = {
         background_color: '#027BFF',
         theme_color: '#027BFF',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/logo.png', // This path is relative to the root of the site.
       },
     },
     {
