465403e4cddd321ef669a6f6b069d7909b205d86
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2020-02-18 20:42:59 +0530

    Gatsby Config Updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,11 +17,40 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
+    'gatsby-plugin-catch-links',
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxwidth: 900
+            }
+          },
+          {
+            resolve: 'gatsby-remark-responsive-iframe',
+            options: {
+              wrapperStyle: 'margin-bottom: 1.0725rem',
+            }
+          },
+          'gatsby-remark-prismjs',
+          'gatsby-remark-copy-linked-files',
+        ]
+      }
+    },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'images',
         path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/content`,
+        name: 'pages',
       },
     },
     {
@@ -52,6 +81,17 @@ module.exports = {
         showSpinner: false,
       },
     },
+    // {
+    //   resolve: 'gatsby-plugin-google-fonts',
+    //   options: {
+    //     fonts: [
+    //       'Abril Fatface',
+    //     ],
+    //     display: 'swap'
+    //   }
+    // },
+
+    // 'gatsby-plugin-feed',
     'gatsby-plugin-robots-txt',
     'gatsby-plugin-sitemap',
     'gatsby-plugin-offline',
