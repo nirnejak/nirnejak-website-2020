affa7c84d55a01b38585ed776757f6c04084ed4e
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2020-02-23 17:45:31 +0530

    Blogs Pages Updated

--- src/pages/blogs.js
+++ src/pages/blogs.js
@@ -1,16 +1,52 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Header from "../components/header"
+import Footer from "../components/footer"
 import SEO from "../components/seo"
+
+import FormBlogImage from "../components/blogs/form_blog"
+import GatsbyPluginBlogImage from "../components/blogs/gatsby_plugins"
+import VuePluginBlogImage from "../components/blogs/vue_plugin"
 
 const Blogs = () => (
   <>
     <SEO title="Blogs" />
     <Header />
-    <h1>Hi from the blogs page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <div className="hero is-fullheight">
+      <div className="hero-body">
+        <section className="container ha">
+          <h1 className="is-huge">Blogs</h1>
+          <br /><br />
+          <div className="image-contents-container">
+            <div className="image-content-container">
+              <a href="https://www.inkoop.io/blog/how-to-ensure-great-user-experience-on-forms/">
+                <FormBlogImage />
+                <br />
+                <p className="has-text-black has-text-weight-bold">Forms that don't suck | How to ensure great user experience on forms</p>
+                <p className="has-text-weight-light" style={{ marginBottom: 5 }}>A guide to building input forms that are easy to use and aesthetically pleasing.</p>
+              </a>
+            </div>
+            <div className="image-content-container">
+              <a href="https://www.inkoop.io/blog/must-have-gatsby-plugins-for-a-great-blog/">
+                <GatsbyPluginBlogImage />
+                <br />
+                <p className="has-text-black has-text-weight-bold">Must have Gatsby plugins for a great blog</p>
+                <p className="has-text-weight-light" style={{ marginBottom: 5 }}>A comprehensive list of extremely useful Gatsby Plugins that can help you build, deploy and monitor a great blog.</p>
+              </a>
+            </div>
+            <div className="image-content-container">
+              <a href="https://www.inkoop.io/blog/building-custom-plugins-for-vue/">
+                <VuePluginBlogImage />
+                <br />
+                <p className="has-text-black has-text-weight-bold">Building custom plugins for Vue.js | Supercharge your Vue.js Development</p>
+                <p className="has-text-weight-light" style={{ marginBottom: 5 }}>A guide to building custom global vue.js plugins.</p>
+              </a>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+    <Footer />
   </>
 )
 
