4af7090a39d633af9ecdb0ab4d2dc57459392773
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2020-02-23 19:00:30 +0530

    Blogs Page: More Blogs Added

---
+++ src/components/blogs/jsdoc_for_react.js
@@ -0,0 +1,21 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+
+const JSDocBlogImage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      placeholderImage: file(relativePath: { eq: "blogs/jsdoc_for_react.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+
+  return <Img fluid={data.placeholderImage.childImageSharp.fluid} style={{ borderRadius: 7 }} />
+}
+
+export default JSDocBlogImage
--- src/pages/blogs.js
+++ src/pages/blogs.js
@@ -1,12 +1,14 @@
 import React from "react"
 
 import Header from "../components/header"
-import Footer from "../components/footer"
 import SEO from "../components/seo"
+import Footer from "../components/footer"
 
 import FormBlogImage from "../components/blogs/form_blog"
 import GatsbyPluginBlogImage from "../components/blogs/gatsby_plugins"
 import VuePluginBlogImage from "../components/blogs/vue_plugin"
+import VuesaxBlogImage from "../components/blogs/vuesax"
+import JSDocBlogImage from "../components/blogs/jsdoc_for_react"
 
 const Blogs = () => (
   <>
@@ -14,12 +16,12 @@ const Blogs = () => (
     <Header />
     <div className="hero is-fullheight">
       <div className="hero-body">
-        <section className="container ha">
+        <section className="container">
           <h1 className="is-huge">Blogs</h1>
           <br /><br />
           <div className="image-contents-container">
             <div className="image-content-container">
-              <a href="https://www.inkoop.io/blog/how-to-ensure-great-user-experience-on-forms/">
+              <a href="https://www.inkoop.io/blog/how-to-ensure-great-user-experience-on-forms/" target="_blank">
                 <FormBlogImage />
                 <br />
                 <p className="has-text-black has-text-weight-bold">Forms that don't suck | How to ensure great user experience on forms</p>
@@ -27,19 +29,37 @@ const Blogs = () => (
               </a>
             </div>
             <div className="image-content-container">
-              <a href="https://www.inkoop.io/blog/must-have-gatsby-plugins-for-a-great-blog/">
+              <a href="https://www.inkoop.io/blog/must-have-gatsby-plugins-for-a-great-blog/" target="_blank">
                 <GatsbyPluginBlogImage />
                 <br />
                 <p className="has-text-black has-text-weight-bold">Must have Gatsby plugins for a great blog</p>
                 <p className="has-text-weight-light" style={{ marginBottom: 5 }}>A comprehensive list of extremely useful Gatsby Plugins that can help you build, deploy and monitor a great blog.</p>
               </a>
             </div>
+          </div>
+          <div className="image-contents-container">
             <div className="image-content-container">
-              <a href="https://www.inkoop.io/blog/building-custom-plugins-for-vue/">
+              <a href="https://www.inkoop.io/blog/building-custom-plugins-for-vue/" target="_blank">
                 <VuePluginBlogImage />
                 <br />
                 <p className="has-text-black has-text-weight-bold">Building custom plugins for Vue.js | Supercharge your Vue.js Development</p>
                 <p className="has-text-weight-light" style={{ marginBottom: 5 }}>A guide to building custom global vue.js plugins.</p>
+              </a>
+            </div>
+            <div className="image-content-container">
+              <a href="https://www.inkoop.io/blog/vuesax-the-all-new-vue-component-library/" target="_blank">
+                <VuesaxBlogImage />
+                <br />
+                <p className="has-text-black has-text-weight-bold">Vuesax - The all new Vue Component Library with Material Design</p>
+                <p className="has-text-weight-light" style={{ marginBottom: 5 }}>Vuesax - The all new Vue Component Library with Material Design</p>
+              </a>
+            </div>
+            <div className="image-content-container">
+              <a href="https://www.inkoop.io/blog/a-guide-to-js-docs-for-react-js/" target="_blank">
+                <JSDocBlogImage />
+                <br />
+                <p className="has-text-black has-text-weight-bold">A Guide to using JSDoc for React.js | Better Documentation in React</p>
+                <p className="has-text-weight-light" style={{ marginBottom: 5 }}>A Guide to using JSDoc for generating documentation for react.js applications.</p>
               </a>
             </div>
           </div>
