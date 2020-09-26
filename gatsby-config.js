880c8dde22543cade5615b274f58e508d73eac28
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2020-09-27 05:08:38 +0530

    ESLint, Prettier, Pre-commit Setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,92 +2,79 @@ require(`dotenv`).config({ path: `.env` })
 
 module.exports = {
   siteMetadata: {
-    title: 'Jitendra Nirnejak - Portfolio',
-    description: 'Portfolio website of Jitendra Nirnejak, I am a full-stack developer, UI/UX Designer, and Tech Blogger.',
-    author: 'Jitendra Nirnejak',
+    title: "Jitendra Nirnejak - Portfolio",
+    description:
+      "Portfolio website of Jitendra Nirnejak, I am a full-stack developer, UI/UX Designer, and Tech Blogger.",
+    author: "Jitendra Nirnejak",
 
-    siteLanguage: 'en',
-    siteUrl: 'https://nirnejak.netlify.com',
-    siteImage: 'https://nirnejak.netlify.com/public/cover.png',
+    siteLanguage: "en",
+    siteUrl: "https://nirnejak.netlify.com",
+    siteImage: "https://nirnejak.netlify.com/public/cover.png",
 
     social: {
-      github: 'nirnejak',
-      codepen: 'nirnejak',
-      dribbble: 'nirnejak',
-      behance: 'nirnejak',
-      linkedin: 'nirnejak',
-      twitter: 'jeetnirnejak',
+      github: "nirnejak",
+      codepen: "nirnejak",
+      dribbble: "nirnejak",
+      behance: "nirnejak",
+      linkedin: "nirnejak",
+      twitter: "jeetnirnejak",
     },
 
     facebook_app: {
       fb_admin_id: process.env.FB_ADMINS_ID,
-      fb_app_id: process.env.FB_APP_ID
+      fb_app_id: process.env.FB_APP_ID,
     },
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-catch-links',
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-catch-links",
     {
-      resolve: 'gatsby-transformer-remark',
+      resolve: "gatsby-transformer-remark",
       options: {
         plugins: [
           {
-            resolve: 'gatsby-remark-images',
+            resolve: "gatsby-remark-images",
             options: {
-              maxwidth: 900
-            }
+              maxwidth: 900,
+            },
           },
           {
-            resolve: 'gatsby-remark-responsive-iframe',
+            resolve: "gatsby-remark-responsive-iframe",
             options: {
-              wrapperStyle: 'margin-bottom: 1.0725rem',
-            }
+              wrapperStyle: "margin-bottom: 1.0725rem",
+            },
           },
-          'gatsby-remark-prismjs',
-          'gatsby-remark-copy-linked-files',
-        ]
-      }
+          "gatsby-remark-prismjs",
+          "gatsby-remark-copy-linked-files",
+        ],
+      },
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: 'images',
+        name: "images",
         path: `${__dirname}/src/images`,
       },
     },
-    // {
-    //   resolve: 'gatsby-source-filesystem',
-    //   options: {
-    //     path: `${__dirname}/src/content`,
-    //     name: 'pages',
-    //   },
-    // },
     {
-      resolve: 'gatsby-plugin-google-analytics',
+      resolve: "gatsby-plugin-google-analytics",
       options: {
         trackingId: process.env.GOOGLE_ANALYTICS_ID,
       },
     },
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
-    'gatsby-plugin-sass',
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-sass",
     {
-      resolve: 'gatsby-plugin-manifest',
+      resolve: "gatsby-plugin-manifest",
       options: {
-        name: 'Jitendra Nirnejak',
-        short_name: 'nirnejak',
-        start_url: '/',
-        background_color: '#027BFF',
-        theme_color: '#027BFF',
-        display: 'minimal-ui',
-        icon: 'src/images/logo.png', // This path is relative to the root of the site.
-      },
-    },
-    {
-      resolve: 'gatsby-plugin-nprogress',
-      options: {
-        color: '#027BFF',
-        showSpinner: false,
+        name: "Jitendra Nirnejak",
+        short_name: "nirnejak",
+        start_url: "/",
+        background_color: "#027BFF",
+        theme_color: "#027BFF",
+        display: "minimal-ui",
+        icon: "src/images/logo.png", // This path is relative to the root of the site.
       },
     },
     // {
@@ -101,9 +88,9 @@ module.exports = {
     // },
 
     // 'gatsby-plugin-feed',
-    'gatsby-plugin-robots-txt',
-    'gatsby-plugin-sitemap',
-    'gatsby-plugin-offline',
-    'gatsby-plugin-netlify',
+    "gatsby-plugin-robots-txt",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-offline",
+    "gatsby-plugin-netlify",
   ],
 }
