bdf81c46c5081c1a908c61c4e3dfef64baa577db
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2021-02-14 18:11:59 +0530

    BlogPage: refactored

--- src/pages/blogs.js
+++ src/pages/blogs.js
@@ -14,7 +14,7 @@ import VuesaxBlogImage from "../components/blogs/vuesax"
 import JSDocBlogImage from "../components/blogs/jsdoc_for_react"
 import AdminBroImage from "../components/blogs/admin_bro"
 
-const blogs = () => (
+const BlogsPage = () => (
   <>
     <SEO title="Blogs" />
     <Header />
@@ -29,165 +29,22 @@ const blogs = () => (
           <br />
           <br />
           <div className="image-contents-container">
-            <Fade>
-              <div className="image-content-container">
-                <a
-                  href="https://www.inkoop.io/blog/how-to-integrate-gatsby-with-shopify-store/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <GatsbyShopifyBlogImage />
-                  <br />
-                  <p className="has-text-black has-text-weight-bold">
-                    How to integrate Gatsby with Shopify Store
-                  </p>
-                  <p className="has-text-weight-light has-text-secondary">
-                    Integrate your Shopify Store with Gatsby Site, build
-                    e-commerce website using Gatsby and Shopify
-                  </p>
-                </a>
-              </div>
-            </Fade>
-            <Fade>
-              <div className="image-content-container">
-                <a
-                  href="https://www.inkoop.io/blog/setup-eslint-for-react-with-prettier-pre-commit-and-vscode/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <ESLintBlogImage />
-                  <br />
-                  <p className="has-text-black has-text-weight-bold">
-                    Setup ESLint for React with Prettier, pre-commit and VSCode
-                  </p>
-                  <p className="has-text-weight-light has-text-secondary">
-                    A Guide to setting up ESLint for React with Prettier,
-                    Pre-commit Hook and Visual Studio Code for better
-                    development with linting and code formatting that works with
-                    your editor.
-                  </p>
-                </a>
-              </div>
-            </Fade>
-            <Fade>
-              <div className="image-content-container">
-                <a
-                  href="https://www.inkoop.io/blog/how-to-ensure-great-user-experience-on-forms/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <FormBlogImage />
-                  <br />
-                  <p className="has-text-black has-text-weight-bold">
-                    Forms that don&apos;t suck | How to ensure great user
-                    experience on forms
-                  </p>
-                  <p className="has-text-weight-light">
-                    A guide to building input forms that are easy to use and
-                    aesthetically pleasing.
-                  </p>
-                </a>
-              </div>
-            </Fade>
-            <Fade>
-              <div className="image-content-container">
-                <a
-                  href="https://www.inkoop.io/blog/must-have-gatsby-plugins-for-a-great-blog/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <GatsbyPluginBlogImage />
-                  <br />
-                  <p className="has-text-black has-text-weight-bold">
-                    Must have Gatsby plugins for a great blog
-                  </p>
-                  <p className="has-text-weight-light">
-                    A comprehensive list of extremely useful Gatsby Plugins that
-                    can help you build, deploy and monitor a great blog.
-                  </p>
-                </a>
-              </div>
-            </Fade>
-            <Fade>
-              <div className="image-content-container">
-                <a
-                  href="https://www.inkoop.io/blog/building-custom-plugins-for-vue/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <VuePluginBlogImage />
-                  <br />
-                  <p className="has-text-black has-text-weight-bold">
-                    Building custom plugins for Vue.js | Supercharge your Vue.js
-                    Development
-                  </p>
-                  <p className="has-text-weight-light">
-                    A guide to building custom global vue.js plugins.
-                  </p>
-                </a>
-              </div>
-            </Fade>
-            <Fade>
-              <div className="image-content-container">
-                <a
-                  href="https://www.inkoop.io/blog/vuesax-the-all-new-vue-component-library/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <VuesaxBlogImage />
-                  <br />
-                  <p className="has-text-black has-text-weight-bold">
-                    Vuesax - The all new Vue Component Library with Material
-                    Design
-                  </p>
-                  <p className="has-text-weight-light">
-                    Vuesax - The all new Vue Component Library with Material
-                    Design
-                  </p>
-                </a>
-              </div>
-            </Fade>
-            <Fade>
-              <div className="image-content-container">
-                <a
-                  href="https://www.inkoop.io/blog/a-guide-to-js-docs-for-react-js/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <JSDocBlogImage />
-                  <br />
-                  <p className="has-text-black has-text-weight-bold">
-                    A Guide to using JSDoc for React.js | Better Documentation
-                    in React
-                  </p>
-                  <p className="has-text-weight-light">
-                    A Guide to using JSDoc for generating documentation for
-                    react.js applications.
-                  </p>
-                </a>
-              </div>
-            </Fade>
-            <Fade>
-              <div className="image-content-container">
-                <a
-                  href="https://www.inkoop.io/blog/adminbro-a-modern-admin-panel-for-node-js/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <AdminBroImage />
-                  <br />
-                  <p className="has-text-black has-text-weight-bold">
-                    AdminBro - a Modern Admin Panel for Node JS | React based
-                    Admin Panel
-                  </p>
-                  <p className="has-text-weight-light">
-                    AdminBro is a React JS based admin panel for Node JS. We
-                    talk about how to set it up and use it in Node JS
-                    applications with MongoDB and SQL Databases.
-                  </p>
-                </a>
-              </div>
-            </Fade>
+            {blogs.map((blog, index) => (
+              <Fade key={index}>
+                <div className="image-content-container">
+                  <a href={blog.url} target="_blank" rel="noopener noreferrer">
+                    {blog.image}
+                    <br />
+                    <p className="has-text-black has-text-weight-bold">
+                      {blog.title}
+                    </p>
+                    <p className="has-text-weight-light has-text-secondary">
+                      {blog.description}
+                    </p>
+                  </a>
+                </div>
+              </Fade>
+            ))}
           </div>
         </section>
       </div>
@@ -196,4 +53,87 @@ const blogs = () => (
   </>
 )
 
-export default blogs
+const blogs = [
+  {
+    title: "How to integrate Gatsby with Shopify Store",
+    description:
+      "Integrate your Shopify Store with Gatsby Site, build e-commerce website using Gatsby and Shopify.",
+    date: "11 Feb 2021",
+    author: "Jitendra Nirnejak",
+    url:
+      "https://www.inkoop.io/blog/how-to-integrate-gatsby-with-shopify-store/",
+    image: <GatsbyShopifyBlogImage />,
+  },
+  {
+    title: "Setup ESLint for React with Prettier, pre-commit and VSCode",
+    description:
+      "A Guide to setting up ESLint for React with Prettier, Pre-commit Hook and Visual Studio Code for better development with linting and code formatting that works with your editor.",
+    date: "19 Nov 2020",
+    author: "Jitendra Nirnejak",
+    url:
+      "https://www.inkoop.io/blog/setup-eslint-for-react-with-prettier-pre-commit-and-vscode/",
+    image: <ESLintBlogImage />,
+  },
+  {
+    title:
+      "Forms that don't suck | How to ensure great user experience on forms",
+    description:
+      "A guide to building input forms that are easy to use and aesthetically pleasing. UX design tips & tricks to provide better visuals & great user experience.",
+    date: "10 Feb 2020",
+    author: "Jitendra Nirnejak",
+    url:
+      "https://www.inkoop.io/blog/how-to-ensure-great-user-experience-on-forms/",
+    image: <FormBlogImage />,
+  },
+  {
+    title: "Must have Gatsby plugins for a great blog",
+    description:
+      "A comprehensive list of extremely useful Gatsby Plugins that can help you build, deploy and monitor a great blog.",
+    date: "31 Jan 2020",
+    author: "Jitendra Nirnejak",
+    url:
+      "https://www.inkoop.io/blog/must-have-gatsby-plugins-for-a-great-blog/",
+    image: <GatsbyPluginBlogImage />,
+  },
+  {
+    title: "Building custom plugins for Vue.js | Supercharge Development",
+    description:
+      "A guide to building custom global vue.js plugins for app-wide accessible components, customs filters and custom directives.",
+    date: "31 Dec 2019",
+    author: "Jitendra Nirnejak",
+    url: "https://www.inkoop.io/blog/building-custom-plugins-for-vue/",
+    image: <VuePluginBlogImage />,
+  },
+  {
+    title: "Vuesax - The all new Vue Component Library",
+    description:
+      "Vuesax - The all-new Vue Component Library. Why Vuesax? How to Install & Integrate Vuesax? How to customize Vuesax Theme?",
+    date: "30 Nov 2019",
+    author: "Jitendra Nirnejak",
+    url: "https://www.inkoop.io/blog/vuesax-the-all-new-vue-component-library/",
+    image: <VuesaxBlogImage />,
+  },
+  {
+    title:
+      "A Guide to using JSDoc for React.js | Better Documentation in React",
+    description:
+      "A Guide to using JSDoc for generating documentation for react.js applications. Find out the benefits of using JSDoc and using JSDoc with React Component.",
+    date: "30 Oct 2019",
+    author: "Jitendra Nirnejak",
+    url: "https://www.inkoop.io/blog/a-guide-to-js-docs-for-react-js/",
+    image: <JSDocBlogImage />,
+  },
+  {
+    title:
+      "AdminBro - a Modern Admin Panel for Node JS | React based Admin Panel",
+    description:
+      "AdminBro is a React JS based admin panel for Node JS. We talk about how to set it up and use it in Node JS applications with MongoDB and SQL Databases.",
+    date: "24 Sep 2021",
+    author: "Jitendra Nirnejak",
+    url:
+      "https://www.inkoop.io/blog/adminbro-a-modern-admin-panel-for-node-js/",
+    image: <AdminBroImage />,
+  },
+]
+
+export default BlogsPage
