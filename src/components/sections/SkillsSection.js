cc6e372764e93b634114518eac80df7d167b554b
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2021-04-01 23:49:53 +0530

    SVG images from require to import

--- src/components/sections/SkillsSection.js
+++ src/components/sections/SkillsSection.js
@@ -1,6 +1,14 @@
 import React from "react"
 import Fade from "react-reveal/Fade"
 
+import ReactLogo from "../../images/skills/reactjs.svg"
+import GatsbyLogo from "../../images/skills/gatsby.svg"
+import NodeLogo from "../../images/skills/nodejs.svg"
+import VueLogo from "../../images/skills/vuejs.svg"
+import FigmaLogo from "../../images/skills/figma.svg"
+import AdobeXDLogo from "../../images/skills/adobe-xd.svg"
+import PhotoshopLogo from "../../images/skills/adobe-photoshop.svg"
+
 const SkillsSection = () => {
   return (
     <section className="hero">
@@ -12,64 +20,40 @@ const SkillsSection = () => {
           <div className="skills-container">
             <Fade>
               <div className="skill-container">
-                <img
-                  src={require("../../images/skills/reactjs.svg")}
-                  alt="React.js"
-                  title="React.js"
-                />
+                <img src={ReactLogo} alt="React.js" title="React.js" />
               </div>
             </Fade>
             <Fade>
               <div className="skill-container">
-                <img
-                  src={require("../../images/skills/gatsby.svg")}
-                  alt="Gatsby"
-                  title="Gatsby"
-                />
+                <img src={GatsbyLogo} alt="Gatsby" title="Gatsby" />
               </div>
             </Fade>
             <Fade>
               <div className="skill-container">
-                <img
-                  src={require("../../images/skills/nodejs.svg")}
-                  alt="Node.js"
-                  title="Node.js"
-                />
+                <img src={NodeLogo} alt="Node.js" title="Node.js" />
               </div>
             </Fade>
             <Fade>
               <div className="skill-container">
-                <img
-                  src={require("../../images/skills/vuejs.svg")}
-                  alt="Vue.js"
-                  title="Vue.js"
-                />
+                <img src={VueLogo} alt="Vue.js" title="Vue.js" />
               </div>
             </Fade>
           </div>
           <div className="skills-container">
             <Fade>
               <div className="skill-container">
-                <img
-                  src={require("../../images/skills/figma.svg")}
-                  alt="Figma"
-                  title="Figma"
-                />
+                <img src={FigmaLogo} alt="Figma" title="Figma" />
+              </div>
+            </Fade>
+            <Fade>
+              <div className="skill-container">
+                <img src={AdobeXDLogo} alt="Adobe XD" title="Adobe XD" />
               </div>
             </Fade>
             <Fade>
               <div className="skill-container">
                 <img
-                  src={require("../../images/skills/adobe-xd.svg")}
-                  alt="Adobe XD"
-                  title="Adobe XD"
-                />
-              </div>
-            </Fade>
-            <Fade>
-              <div className="skill-container">
-                <img
-                  src={require("../../images/skills/adobe-photoshop.svg")}
+                  src={PhotoshopLogo}
                   alt="Adobe Photoshop"
                   title="Adobe Photoshop"
                 />
