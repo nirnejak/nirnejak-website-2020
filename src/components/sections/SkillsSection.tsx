ffb432745b5eca1a0065fd83dd7f563686835171
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2021-06-25 22:23:02 +0530

    React Native Added in Skills

--- src/components/sections/SkillsSection.tsx
+++ src/components/sections/SkillsSection.tsx
@@ -7,6 +7,7 @@ import AdobeXDLogo from "../../images/skills/adobe-xd.svg"
 import FigmaLogo from "../../images/skills/figma.svg"
 import GatsbyLogo from "../../images/skills/gatsby.svg"
 import NodeLogo from "../../images/skills/nodejs.svg"
+import ReactNativeLogo from "../../images/skills/react-native.svg"
 import ReactLogo from "../../images/skills/reactjs.svg"
 import VueLogo from "../../images/skills/vuejs.svg"
 
@@ -22,6 +23,15 @@ const SkillsSection: FC = (): ReactElement => {
             <Fade>
               <div className="skill-container">
                 <img src={ReactLogo} alt="React.js" title="React.js" />
+              </div>
+            </Fade>
+            <Fade>
+              <div className="skill-container">
+                <img
+                  src={ReactNativeLogo}
+                  alt="React Native"
+                  title="React Native"
+                />
               </div>
             </Fade>
             <Fade>
