90087197a85a74714ef7f33d0152f3890cb50816
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2021-06-27 17:23:09 +0530

    Contact Function Updated

--- api/contact.js
+++ api/contact.js
@@ -1,14 +1,19 @@
 module.exports = (req, res) => {
-  res.status(200).json({
-    name: "Jitendra Nirnejak",
-    phone: "7869290297",
-    email: "hello@nirnejak.com",
-    website: "https://nirnejak.com",
-    social: {
-      linkedin: "https://www.linkedin.com/in/nirnejak/",
-      facebook: "https://www.facebook.com/nirnejak",
-      instagram: "https://www.instagram.com/jitendra.nirnejak/",
-      twitter: "https://twitter.com/jeetnirnejak",
-    },
-  })
+  if (req.method === "POST") {
+    console.log(req.body)
+    res.status(201).json({ message: "Success" })
+  } else {
+    res.status(200).json({
+      name: "Jitendra Nirnejak",
+      phone: "7869290297",
+      email: "hello@nirnejak.com",
+      website: "https://nirnejak.com",
+      social: {
+        linkedin: "https://www.linkedin.com/in/nirnejak/",
+        facebook: "https://www.facebook.com/nirnejak",
+        instagram: "https://www.instagram.com/jitendra.nirnejak/",
+        twitter: "https://twitter.com/jeetnirnejak",
+      },
+    })
+  }
 }
