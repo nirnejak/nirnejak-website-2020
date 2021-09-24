c4b562fcad5c54e4e61d9cf6d83001b10aa89d96
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2021-09-25 02:39:34 +0530

    React Native Removed

--- src/components/sections/SkillsSection.tsx
+++ src/components/sections/SkillsSection.tsx
@@ -7,7 +7,6 @@ import AdobeXDLogo from "../../images/skills/adobe-xd.svg"
 import FigmaLogo from "../../images/skills/figma.svg"
 import GatsbyLogo from "../../images/skills/gatsby.svg"
 import NodeLogo from "../../images/skills/nodejs.svg"
-import ReactNativeLogo from "../../images/skills/react-native.svg"
 import ReactLogo from "../../images/skills/reactjs.svg"
 import VueLogo from "../../images/skills/vuejs.svg"
 
@@ -21,15 +20,6 @@ const SkillsSection: React.FC = () => {
             <Fade>
               <div className="skill-container">
                 <img src={ReactLogo} alt="React.js" title="React.js" />
-              </div>
-            </Fade>
-            <Fade>
-              <div className="skill-container">
-                <img
-                  src={ReactNativeLogo}
-                  alt="React Native"
-                  title="React Native"
-                />
               </div>
             </Fade>
             <Fade>
