83a2509070605625d7f71b1529e631cfe53e61a2
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2021-09-25 00:40:26 +0530

    Functions Updated

--- api/contact.js
+++ api/contact.js
@@ -3,17 +3,6 @@ module.exports = (req, res) => {
     console.log(req.body)
     res.status(201).json({ message: "Success" })
   } else {
-    res.status(200).json({
-      name: "Jitendra Nirnejak",
-      phone: "7869290297",
-      email: "hello@nirnejak.com",
-      website: "https://nirnejak.com",
-      social: {
-        linkedin: "https://www.linkedin.com/in/nirnejak/",
-        facebook: "https://www.facebook.com/nirnejak",
-        instagram: "https://www.instagram.com/jitendra.nirnejak/",
-        twitter: "https://twitter.com/jeetnirnejak",
-      },
-    })
+    res.status(405).send("Invalid method")
   }
 }
