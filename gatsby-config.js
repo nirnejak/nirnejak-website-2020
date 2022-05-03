0efd7fbf649b1033055de2224b3e925f148f6c81
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2022-05-03 22:49:20 +0530

    Google Analytics to Gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,9 +54,9 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: "gatsby-plugin-google-gtag",
       options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+        trackingIds: [process.env.GOOGLE_ANALYTICS_ID],
       },
     },
     {
