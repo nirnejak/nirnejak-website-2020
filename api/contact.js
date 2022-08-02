4193dd190bdf3a9e38554605cc828c2f34074b6e
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2022-08-02 22:49:31 +0530

    Contact API removed

--- api/contact.js
+++
@@ -1,8 +0,0 @@
-module.exports = (req, res) => {
-  if (req.method === "POST") {
-    console.log(req.body)
-    res.status(201).json({ message: "Success" })
-  } else {
-    res.status(405).json({ message: "Invalid method" })
-  }
-}
