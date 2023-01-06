12a4e86d7d03e67909002552c40374d12fb96be2
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2023-01-06 19:51:35 +0530

    Footer: PH link added

--- src/components/molecules/Footer.tsx
+++ src/components/molecules/Footer.tsx
@@ -19,6 +19,18 @@ const Footer: React.FC = () => {
     hidden: { opacity: 0, scale: 0 },
   }
 
+  const [iconColor, setIconColor] = React.useState("#000")
+
+  React.useEffect(() => {
+    const media = window.matchMedia("(prefers-color-scheme: dark)")
+
+    media.matches && setIconColor("#fff")
+
+    media.addEventListener("change", (event) => {
+      setIconColor(event.matches ? "#fff" : "#000")
+    })
+  }, [])
+
   return (
     <div
       className="has-text-centered"
@@ -64,7 +76,7 @@ const Footer: React.FC = () => {
         <motion.div
           animate={controls}
           initial="hidden"
-          transition={{ delay: 0.1, duration: 0.3 }}
+          transition={{ delay: 0.05, duration: 0.3 }}
           variants={variants}
           className="icon-container"
           style={{ margin: "20px 15px" }}
@@ -81,7 +93,7 @@ const Footer: React.FC = () => {
         <motion.div
           animate={controls}
           initial="hidden"
-          transition={{ delay: 0.2, duration: 0.3 }}
+          transition={{ delay: 0.1, duration: 0.3 }}
           variants={variants}
           className="icon-container"
           style={{ margin: "20px 15px" }}
@@ -98,7 +110,7 @@ const Footer: React.FC = () => {
         <motion.div
           animate={controls}
           initial="hidden"
-          transition={{ delay: 0.3, duration: 0.3 }}
+          transition={{ delay: 0.15, duration: 0.3 }}
           variants={variants}
           className="icon-container"
           style={{ margin: "20px 15px" }}
@@ -110,6 +122,32 @@ const Footer: React.FC = () => {
             rel="noopener noreferrer"
           >
             <LinkedinFill />
+          </a>
+        </motion.div>
+        <motion.div
+          animate={{ opacity: 1, scale: 1 }}
+          initial={{ opacity: 0, scale: 0 }}
+          transition={{ delay: 0.2, duration: 0.3 }}
+          className="icon-container"
+          style={{ margin: "20px 15px" }}
+        >
+          <a
+            href="https://www.producthunt.com/@nirnejak"
+            title="Jitendra Nirnejak's Product Hunt Profile"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <svg
+              width={24}
+              height={24}
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+            >
+              <path
+                fill={iconColor}
+                d="M326.3 218.8c0 20.5-16.7 37.2-37.2 37.2h-70.3v-74.4h70.3c20.5 0 37.2 16.7 37.2 37.2zM504 256c0 137-111 248-248 248S8 393 8 256 119 8 256 8s248 111 248 248zm-128.1-37.2c0-47.9-38.9-86.8-86.8-86.8H169.2v248h49.6v-74.4h70.3c47.9 0 86.8-38.9 86.8-86.8z"
+              />
+            </svg>
           </a>
         </motion.div>
       </div>
